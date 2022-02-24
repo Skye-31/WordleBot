2bca5f6e0dec74a91073b62b10ed3a54f70fa6a3
Eve <46286597+Skye-31@users.noreply.github.com>
2022-02-24 17:30:17 +0000

    Change tests slightly

--- main.go
+++ main.go
@@ -76,6 +76,6 @@ func main() {
 
 	logger.Infof("WordleBot is now running. Press CTRL-C to exit.")
 	s := make(chan os.Signal, 1)
-	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-s
 }
